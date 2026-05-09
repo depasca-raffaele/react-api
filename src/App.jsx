3548e793524748907a92cbae6219a1a53fcd65b8
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-05-09 14:54:42 +0200

    recupero lista attori dalla API

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { useEffect } from 'react'
 
-  return (
-    <>
-     
-    </>
-  )
+function App() {
+  useEffect(() => {
+    fetch('https://lanciweb.github.io/demo/api/actors/')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error('Errore nel recupero attori:', error)
+      })
+  }, [])
+
+  return <></>
 }
 
 export default App
